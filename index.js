dd9f0e0f65a59bded7603cf72eec48c5d3e75333
russash <127902586+russash@users.noreply.github.com>
2023-06-05 20:45:09 +0100

    Update index.js
    Prettier auto format charset

--- index.js
+++ index.js
@@ -1,24 +1,113 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
-let passOneEl = document.getElementById("passone-el")
-let passTwoEl = document.getElementById("passtwo-el")
-let passwordLength = 15
+let passOneEl = document.getElementById("passone-el");
+let passTwoEl = document.getElementById("passtwo-el");
+let passwordLength = 15;
 
-function genRandom(){
-    let random = Math.floor(Math.random()*characters.length)
-    return characters[random]
+function genRandom() {
+  let random = Math.floor(Math.random() * characters.length);
+  return characters[random];
 }
 
 function generatePasswords() {
-    let passOne = ""
-    let passTwo = ""
-    for (let i = 0; i < passwordLength; i++) {
-        passOne += genRandom()
-        passTwo += genRandom()
-    }
-    passOneEl.textContent = passOne
-    passTwoEl.textContent = passTwo
+  let passOne = "";
+  let passTwo = "";
+  for (let i = 0; i < passwordLength; i++) {
+    passOne += genRandom();
+    passTwo += genRandom();
+  }
+  passOneEl.textContent = passOne;
+  passTwoEl.textContent = passTwo;
 }
-
-
